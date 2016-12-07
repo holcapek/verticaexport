748a6c1f6dd7247e99ce09df3f72257b45983c99
Jan Holcapek <jan.holcapek@hhel6rt.int.na.intgdc.com>
2016-12-07 10:10:51 +0100

    Fix ddl

--- ddl/install.sql
+++ ddl/install.sql
@@ -1,3 +1,3 @@
 \set libfile '''/opt/vertica/packages/unload/lib/libunload.so'''
 CREATE LIBRARY unload_lib AS :libfile LANGUAGE 'C++';
-CREATE OR REPLACE FUNCTION unload AS LANGUAGE 'C++' NAME 'unload' LIBRARY unload_lib FENCED;
+CREATE TRANSFORM FUNCTION unload AS LANGUAGE 'C++' NAME 'unload_factory' LIBRARY unload_lib FENCED;
--- ddl/isinstalled.sql
+++ ddl/isinstalled.sql
@@ -1,4 +1,5 @@
 SELECT (COUNT(0) = 1)
 FROM   user_libraries
 WHERE  user_libraries.lib_name = 'unload_lib'
-AND    (user_libraries.md5_sum = '3d990613eb18f24bccd6035b0530da6a' or public.length('3d990613eb18f24bccd6035b0530da6a') = 7);
+-- AND    (user_libraries.md5_sum = '3d990613eb18f24bccd6035b0530da6a' or public.length('3d990613eb18f24bccd6035b0530da6a') = 7);
+;
