123dba1707681ab6ebe23dd8d5271b3eaea98c27
Jan Holcapek <holcapek@gmail.com>
2016-12-09 15:12:55 +0100

    work in progress

--- unload.cpp
+++ unload.cpp
@@ -1,5 +1,11 @@
 // vim:ts=4
 
+/*
+ * Vertica only supports x86_64
+ * size_t = unsigned long long = 8 bytes
+ * - column count
+ */
+
 // PATH_MAX
 #include <linux/limits.h>
 
@@ -59,10 +65,60 @@ private:
 	size_t num_column;
 
 	char* buffer;
-	size_t current_offset; // last byte of the record
-	size_t last_record_offset; // last byte of previous record
+	long int current_offset;     // last byte of the record
+	long int last_record_offset; // last byte of previous record
 
-	void serialize_varchar(ServerInterface& si, PartitionReader& pr, size_t ci) {
+	inline void
+	serialize_null(
+		ServerInterface& si
+	) {
+		if (current_offset + 1 <= BUFFER_SIZE) {
+			buffer[current_offset] = FIELD_DELIMITER_CHAR;
+			current_offset += 1;
+		} else {
+			write_to_file(si, buffer, last_record_offset+1);
+			reset_last_record_offset(si);
+
+			buffer[current_offset] = FIELD_DELIMITER_CHAR;
+			current_offset += 1;
+		}
+	}
+
+	void
+	serialize_int(
+		ServerInterface& si,
+		PartitionReader& pr,
+		size_t ci
+	) {
+		if (pr.isNull(ci)) {
+			serialize_null(si);
+			return;
+		}
+
+		// MAX_VINT = 9223372036854775807
+		// 1 is for sign,
+		// 19 for max number of digits,
+		// 1 for comma
+		// 1 for null byte by sprintf
+		if (current_offset + 1 + 19 + 1 + 1 <= BUFFER_SIZE) {
+			int len = sprintf(buffer+current_offset, "%lli%c", *pr.getIntPtr(ci), FIELD_DELIMITER_CHAR);
+			current_offset += len;
+		} else {
+			write_to_file(si, buffer, last_record_offset + 1);
+			reset_last_record_offset(si);
+
+			int len = sprintf(buffer+current_offset, "%lli%c", *pr.getIntPtr(ci), FIELD_DELIMITER_CHAR);
+			current_offset += len;
+		}
+	}
+
+
+	void
+	serialize_varchar(
+		ServerInterface& si,
+		PartitionReader& pr,
+		size_t ci
+	) {
 		
 		if (pr.isNull(ci)) {
 			serialize_null(si);
@@ -70,67 +126,37 @@ private:
 		}
 
 		VString s = pr.getStringRef(ci);
-		vsize len = s.length();
+		char* data = s.data();
+		unsigned int len = s.length();
 
+		// 1 is for field separator
 		size_t space_avail = BUFFER_SIZE-current_offset-1;
+
 		size_t len_wrote = csv_write2(
 			buffer+current_offset,
 			space_avail,
-			s.data(),
+			data,
 			len,
 			QUOTE_CHAR
 		);
-		LOG(si, "pass#1 str=[%s] len=%u avail=%li len_wrote=%lu", s.str().c_str(), len, space_avail, len_wrote);
-		// 1 for field delim
-		if (len_wrote >= space_avail) {
+
+		if (len_wrote > space_avail) {
+			// TODO what if len_wrote > BUFFER_SIZE
+
 			write_to_file(si, buffer, last_record_offset+1);
 			reset_last_record_offset(si);
-			// try again
+
 			len_wrote = csv_write2(
 				buffer+current_offset,
 				space_avail,
-				s.data(),
+				data,
 				len,
 				QUOTE_CHAR
 			);
-			LOG(si, "pass#2 str=[%s] len=%u avail=%li len_wrote=%lu", s.str().c_str(), len, space_avail, len_wrote);
 		}
 		current_offset += len_wrote;
 		buffer[current_offset] = FIELD_DELIMITER_CHAR;
 		current_offset += 1;
-		LOG(si, "end last_record_offset=%lu current_offset=%lu", last_record_offset, current_offset);
-	}
-
-	inline void serialize_null(ServerInterface& si) {
-		if (current_offset + 1 <= BUFFER_SIZE) {
-			buffer[current_offset] = QUOTE_CHAR;
-			current_offset += 1;
-		} else {
-			write_to_file(si, buffer, last_record_offset+1);
-			reset_last_record_offset(si);
-			buffer[current_offset] = QUOTE_CHAR;
-			current_offset += 1;
-		}
-	}
-
-	void serialize_int(ServerInterface& si, PartitionReader& pr, size_t ci) {
-		if (pr.isNull(ci)) {
-			serialize_null(si);
-			return;
-		}
-		// MAX_VINT = 9223372036854775807
-		// thats 20 bytes at most: 19 digits, optional -, null byte
-		LOG(si, "last_record_offset=%li current_offset=%li", last_record_offset, current_offset);
-		// 1:minus,19:longestvint,1:comma,1:nullbyte
-		if (current_offset + 1 + 19 + 1 + 1<= BUFFER_SIZE) {
-			int len = sprintf(buffer+current_offset, "%lli%c", *pr.getIntPtr(ci), FIELD_DELIMITER_CHAR);
-			current_offset += len;
-		} else {
-			write_to_file(si, buffer, last_record_offset + 1);
-			reset_last_record_offset(si);
-			int len = sprintf(buffer+current_offset, "%lli%c", *pr.getIntPtr(ci), FIELD_DELIMITER_CHAR);
-			current_offset += len;
-		}
 	}
 
 	inline void
@@ -138,7 +164,6 @@ private:
 			memcpy(buffer, buffer+last_record_offset+1, current_offset - last_record_offset);
 			current_offset = current_offset - last_record_offset;
 			last_record_offset = -1;
-			LOG(si, "reset last_record_offset=%li current_offset=%li", last_record_offset, current_offset);
 	}
 
 	inline void
@@ -221,6 +246,12 @@ public:
 
 	}
 
+	void
+	mark_end_of_record() {
+		buffer[current_offset-1] = RECORD_DELIMITER_CHAR;
+		last_record_offset = current_offset - 1;
+	}
+
 	virtual void
 	processPartition (
 		ServerInterface &srvInterface,
@@ -230,15 +261,16 @@ public:
 		LOG(srvInterface, "");
 
 		try {
+
 			do {
 				for (size_t i = 0; i < num_column; i++) {
 					(this->*serializer[i])(srvInterface, input_reader, i);
 				}
-				LOG(srvInterface, "end_of_record_before last_record_offset=%li current_offset=%lu",
-					last_record_offset, current_offset);
-				buffer[current_offset-1] = RECORD_DELIMITER_CHAR;
-				last_record_offset = current_offset - 1;
+
+				mark_end_of_record();
+
 			} while (input_reader.next());
+
         } catch(exception& e) {
             vt_report_error(0, "Exception while processing partition: [%s]", e.what());
         }
@@ -297,8 +329,6 @@ class unload_factory : public TransformFunctionFactory {
 		LOG(srvInterface, "");
 
 		parameterTypes.addVarchar(PATH_MAX, "outfile");
-		// gz, bz2
-		parameterTypes.addVarchar(32, "compress");
 	}
 };
 
--- unload.h
+++ unload.h
@@ -1,12 +1,13 @@
 #ifndef _UNLOAD_H
 #define _UNLOAD_H
 
+#define DEBUG 1
 #define LOG(si,format,args...) \
-	do { \
+	if (DEBUG) { \
 		pid_t pid = getpid(); \
 		pid_t tid = syscall(SYS_gettid); \
 		(si).log("p=%5u t=%5u f=%s " format, pid, tid, __FUNCTION__, ##args); \
-	} while (0)
+	}
 
 #define MUTEX(m)   pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 #define LOCKM(m)   pthread_mutex_lock(&m)
